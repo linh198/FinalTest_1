c5c95550177dd382874369d09b6612586e461298
May <ADMIN@MAY>
2023-05-28 00:27:09 +0700

    Add project files.

---
+++ FinalTest_1/Controllers/ThongtinController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalTest_1.Controllers
+{
+    public class ThongtinController : Controller
+    {
+        public IActionResult Hienthithongtin()
+        {
+            return View();
+        }
+    }
+}
